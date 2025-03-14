5bf51e63ca671b3cfa94ab0dc35382bae7bd683a
jcasben <jcasben02@gmail.com>
2025-03-14 10:53:04 +0100

    [UPDATE]: refactored to use new title component

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -3,21 +3,23 @@ import {ModeService} from '../../services/mode.service';
 import {NgClass} from '@angular/common';
 import {ButtonComponent} from '../../components/button/button.component';
 import {RouterLink} from '@angular/router';
+import {TitleComponent} from '../../components/title/title.component';
 
 @Component({
   selector: 'blearn-header',
   imports: [
     NgClass,
     ButtonComponent,
-    RouterLink
+    RouterLink,
+    TitleComponent
   ],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
   private modeService = inject(ModeService);
 
-  headerText2 = computed(() => (this.modeService.getMode() === 'student' ? 'BLearn' : 'BLearn Teachers'));
-  headerColor2 = computed(() => (this.modeService.getMode() === 'student' ? 'text-student' : 'text-teacher'));
+  headerText = computed(() => (this.modeService.getMode() === 'student' ? 'BLearn' : 'BLearn Teachers'));
+  headerColor = computed(() => (this.modeService.getMode() === 'student' ? 'text-student' : 'text-teacher'));
 
   switchMode() {
     const nextMode = this.modeService.getMode() === 'student' ? 'teacher' : 'student';
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import {Component, computed, inject, signal} from '@angular/core';
 import {ModeService} from "../../services/mode.service";
 import {ButtonComponent} from '../../components/button/button.component';
+import {TitleComponent} from '../../components/title/title.component';
 
 @Component({
   selector: 'blearn-home',
   imports: [
-    ButtonComponent
+    ButtonComponent,
+    TitleComponent
   ],
   templateUrl: './home.component.html',
 })

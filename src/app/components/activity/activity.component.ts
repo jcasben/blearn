eb8162c2f193c506180ec6e9b601af1f69c860eb
jcasben <jcasben02@gmail.com>
2025-03-15 23:49:32 +0100

    [UPDATE]: added navigation to detail page when clicking on activity card

--- src/app/components/activity/activity.component.ts
+++ src/app/components/activity/activity.component.ts
@@ -1,11 +1,19 @@
-import {Component, Input} from '@angular/core';
+import {Component, inject, Input} from '@angular/core';
 import {Activity} from '../../models/activity';
+import {Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'blearn-activity',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './activity.component.html',
 })
 export class ActivityComponent {
+  private router = inject(Router);
   @Input() activity!: Activity
+
+  navigateToDetail() {
+    this.router.navigate([`/activity/${this.activity.id}`]).then();
+  }
 }

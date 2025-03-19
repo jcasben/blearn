5da29f35d9d2a070e659c2eac88e4e1b83d97fb1
jcasben <jcasben02@gmail.com>
2025-03-19 11:43:04 +0100

    [NEW]: added possibility to import task from file

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, effect, inject, OnInit, signal} from '@angular/core';
+import {Component, effect, ElementRef, inject, OnInit, signal, ViewChild} from '@angular/core';
 import {ModeService} from "../../services/mode.service";
 import {ButtonComponent} from '../../components/button/button.component';
 import {TitleComponent} from '../../components/title/title.component';
@@ -20,6 +20,8 @@ export class HomeComponent {
   protected modeService = inject(ModeService);
   protected activityService = inject(ActivityService);
 
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
+
   protected activityList = signal<Activity[]>([]);
 
   constructor() {
@@ -40,6 +42,47 @@ export class HomeComponent {
       };
       this.activityService.addActivity(newActivity);
       this.activityList.set(this.activityService.loadActivities());
+    } else {
+      this.fileInput.nativeElement.click();
+    }
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (!input.files || input.files.length === 0) return;
+
+    const file = input.files[0];
+    if (!file.name.endsWith('.blearn')) {
+      alert('Invalid file type. Please upload a .blearn file');
+      return;
+    }
+
+    let newActivity: Activity;
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      try {
+        const jsonData = JSON.parse(e.target?.result as string);
+        newActivity = this.mapToActivity(jsonData);
+        this.activityService.addActivity(newActivity);
+        this.activityList.set(this.activityService.loadActivities());
+      } catch (error) {
+        console.error('Error parsing the file: ', error);
+        alert('Invalid file content. Please, try again with a BLearn activity file');
+      }
+    };
+
+    reader.readAsText(file);
+  }
+
+  private mapToActivity(jsonData: any): Activity {
+    return {
+      id: jsonData.id,
+      title: jsonData.title,
+      description: jsonData.description,
+      dueDate: jsonData.dueDate,
+      workspace: jsonData.workspace,
     }
   }
 }

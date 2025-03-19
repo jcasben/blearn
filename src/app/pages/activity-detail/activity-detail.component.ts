909fbda560300dd229cf4835a4a14be9905f20dc
jcasben <jcasben02@gmail.com>
2025-03-19 11:42:12 +0100

    [UPDATE]: added provisional button to download file

--- src/app/pages/activity-detail/activity-detail.component.ts
+++ src/app/pages/activity-detail/activity-detail.component.ts
@@ -6,18 +6,24 @@ import {map} from 'rxjs';
 import {BlocklyEditorComponent} from '../../components/blockly-editor/blockly-editor.component';
 import {Activity} from '../../models/activity';
 import {FormsModule} from '@angular/forms';
+import {ModeService} from '../../services/mode.service';
+import {TitleComponent} from '../../components/title/title.component';
+import {ButtonComponent} from '../../components/button/button.component';
 
 @Component({
   selector: 'blearn-activity-detail',
   imports: [
     BlocklyEditorComponent,
-    FormsModule
+    FormsModule,
+    TitleComponent,
+    ButtonComponent
   ],
   templateUrl: './activity-detail.component.html',
 })
 export class ActivityDetailComponent {
   private route = inject(ActivatedRoute);
   private activityService = inject(ActivityService);
+  protected modeService = inject(ModeService);
   private router = inject(Router);
 
   @ViewChild(BlocklyEditorComponent) blocklyEditorComponent!: BlocklyEditorComponent;
@@ -61,5 +67,18 @@ export class ActivityDetailComponent {
     const workspaceJSON = this.blocklyEditorComponent.saveWorkspaceAsJson();
     this.activity.set({ ...this.activity()!, workspace: workspaceJSON });
     this.activityService.updateActivity(this.activityId()!, this.activity()!);
+  }
+
+  downloadFile() {
+    const fileName = `${this.activity()!.title}.blearn`
+    const content = JSON.stringify(this.activity()!);
+
+    const file = new Blob([content], { type: 'application/json' });
+
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(file);
+    link.download = fileName;
+    link.click();
+    link.remove();
   }
 }

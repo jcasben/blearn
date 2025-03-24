4800ecad3f0657a766e97843965b8614463e5e24
jcasben <jcasben02@gmail.com>
2025-03-24 11:19:50 +0100

    [NEW]: created first version of footer

---
+++ src/app/layout/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'blearn-footer',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './footer.component.html',
+})
+export class FooterComponent {
+
+}

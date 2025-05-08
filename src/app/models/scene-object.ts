c4cd0f79d06c14fad5c2abdc1b48d75675a8e721
jcasben <jcasben02@gmail.com>
2025-05-08 13:49:12 +0200

    [UPDATE]: transformed into a class to perform movement actions

--- src/app/models/scene-object.ts
+++ src/app/models/scene-object.ts
@@ -1,8 +1,34 @@
-export interface SceneObject {
-  img: HTMLImageElement;
-  x: number;
-  y: number;
-  rotation: number;
-  width: number;
-  height: number;
+export class SceneObject {
+  constructor(
+    public id: string,
+    public imgSrc: string,
+    public x: number,
+    public y: number,
+    public rotation: number,
+    public width: number,
+    public height: number,
+    public workspace: string,
+    public img?: HTMLImageElement,
+  ) {}
+
+  moveForward(steps: number) {
+    this.x += steps;
+  }
+
+  moveTo(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  setDirection(angle: number) {
+
+  }
+
+  turnLeft(angle: number) {
+
+  }
+
+  turnRight(angle: number) {
+
+  }
 }
